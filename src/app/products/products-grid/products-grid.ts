bb75e68b2887e72e83c08e45896322d6c8973e8f
Charles Donizete Alves dos Santos <82051427+CharlesDonizete@users.noreply.github.com>
2026-04-18 10:04:04 -0300

    Modern Angular Course 13: Writable Signals for Search State

--- src/app/products/products-grid/products-grid.ts
+++ src/app/products/products-grid/products-grid.ts
@@ -2,14 +2,19 @@ import { Component, signal } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../product';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard, MatIconModule],
+  imports: [ProductCard, MatIconModule, MatInputModule, FormsModule, MatFormFieldModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
+  protected readonly searchTerm = signal('');
+
   protected readonly productos = signal<Product[]>([
     {
       id: 1,
@@ -21,7 +26,8 @@ export class ProductsGrid {
     {
       id: 2,
       name: 'Product 2',
-      description: 'Description for Product 2 loren ipsum dolor sit amet, consectetur adipiscing elit. loren ipsum dolor sit amet, consectetur adipiscing elit.',
+      description:
+        'Description for Product 2 loren ipsum dolor sit amet, consectetur adipiscing elit. loren ipsum dolor sit amet, consectetur adipiscing elit.',
       price: 9.99,
     },
     {
@@ -32,4 +38,12 @@ export class ProductsGrid {
       originalPrice: 19.99,
     },
   ]);
+
+  protected clearSearch() {
+    this.searchTerm.set('');
+  }
+
+    protected trimSearch() {
+    this.searchTerm.update((value) => value.trim());
+  }
 }
